9bb50ef1035bae3aa026636e282a9ba4b352b991
youssefadly <youssefadly332@gmail.com>
2025-11-12 14:57:18 +0200

    add app styles

---
+++ lib/Ui/screens/create_new_list_screen.dart
@@ -0,0 +1,16 @@
+import 'package:collaborativeshoppinglistapp/Ui/widgets/create_newList_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class CreateNewListScreen extends StatelessWidget {
+  const CreateNewListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ,
+      ),
+      body:const CreateNewlistScreenBody(),
+    );
+  }
+}
---
+++ lib/Ui/widgets/create_newList_screen_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateNewlistScreenBody extends StatelessWidget {
+  const CreateNewlistScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
